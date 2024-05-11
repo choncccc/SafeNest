7f645db0b1b04932faef75c524ba3aad40ed3ed1
kenj <kenjieramos22@gmail.com>
2024-05-11 22:19:45 +0800

    ThirdTrimester + Week28 - 31

--- app/src/main/java/com/example/safenest/ThirdTrimester.kt
+++ app/src/main/java/com/example/safenest/ThirdTrimester.kt
@@ -5,35 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.safenest.databinding.FragmentReadingBinding
+import com.example.safenest.databinding.FragmentThirdTrimesterBinding
 
 
-class ReadingFragment : Fragment() {
+class ThirdTrimester : Fragment() {
 
-    private lateinit var binding: FragmentReadingBinding
-
+    private lateinit var binding: FragmentThirdTrimesterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
-        binding = FragmentReadingBinding.inflate(inflater, container, false)
+        binding= FragmentThirdTrimesterBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.FirstTrimesterCard.setOnClickListener {
-            updateFragment(FirstTrimesterDescrip())
+        binding.weekTwentyEightDescrip.setOnClickListener {
+            updateFragment(WeekTwentyEightDescrip())
         }
-        binding.SecondTrimesterCard.setOnClickListener{
-            updateFragment(SecondTrimester())
+        binding.weekTwentyNineDescrip.setOnClickListener {
+            updateFragment(WeekTwentyNineDescrip())
         }
-        binding.ThirdTrimesterCard.setOnClickListener{
-            updateFragment(ThirdTrimester())
+        binding.weekThirtyDescrip.setOnClickListener {
+            updateFragment(WeekThirtyDescrip())
         }
-
+        binding.weekThirtyOneDescrip.setOnClickListener {
+            updateFragment(WeekThirtyOneDescrip())
+        }
     }
 
     private fun updateFragment(fragment: Fragment) {
